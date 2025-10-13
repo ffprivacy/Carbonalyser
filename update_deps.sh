e15bb16d55fc86c8cb1815c06090e1f93ef657d0
ffprivacy <ffprivacy@netcourrier.com>
2025-10-13 15:04:06 +0200

    script to automatically update deps

---
+++ update_deps.sh
@@ -0,0 +1,8 @@
+#!/bin/bash
+
+wget https://cdn.jsdelivr.net/npm/bootstrap/dist/css/bootstrap.min.css
+mv bootstrap.min.css lib/bootstrap/
+wget https://cdn.jsdelivr.net/npm/bootstrap/dist/js/bootstrap.min.js
+mv bootstrap.min.js lib/bootstrap/
+wget https://cdn.jsdelivr.net/npm/bootstrap/dist/js/bootstrap.min.js.map
+mv bootstrap.min.js.map lib/bootstrap/
